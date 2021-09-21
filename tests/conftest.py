d829467506a17236e80dd1fe8d859bc11a2ddb50
Dmitrii <swish.ds@gmail.com>
2021-09-21 03:15:12 +0000

    removed auth from tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -46,26 +46,3 @@ def client(app):
 def runner(app):
     # creates a runner that can call the Click commands registered with the application.
     return app.test_cli_runner()
-
-
-# For most of the views, a user needs to be logged in. 
-# way to do this is to make a POST request to the login view with the client.
-class AuthActions(object):
-    # class with methods to do that, and use a fixture to pass it the client for each test.
-    def __init__(self, client):
-        self._client = client
-
-    def login(self, username='test', password='test'):
-        return self._client.post(
-            '/auth/login',
-            data={'username': username, 'password': password}
-        )
-
-    def logout(self):
-        return self._client.get('/auth/logout')
-
-
-@pytest.fixture
-def auth(client):
-    # call auth.login() in a test to log in as the test user, which was inserted as part of the test data in the app fixture.
-    return AuthActions(client)
